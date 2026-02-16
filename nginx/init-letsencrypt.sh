6126db56c5a74ddeb3cb6fbc8c8687825b3402e8
XYTRAX9 <yaroslav.dzhum@yandex.ru>
2026-02-16 11:34:23 +0500

    Исправление букета ошибок

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -2,10 +2,14 @@
 
 # Скрипт для инициализации Let's Encrypt сертификатов
 
-set -e
+# Закомментирован код certbot, так как сервис отключен
+# set -e
 
-echo "### Запуск certbot для получения сертификатов ###"
-docker-compose -f docker-compose.prod.yml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email admin@158.255.1.190.sslip.io --agree-tos --no-eff-email -d 158.255.1.190.sslip.io
-echo "### Перезапуск nginx для применения сертификатов ###"
-docker-compose -f docker-compose.prod.yml restart nginx
-echo "### Готово ###"
+# echo "### Запуск certbot для получения сертификатов ###"
+# docker-compose -f docker-compose.prod.yml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email admin@158.255.1.190.sslip.io --agree-tos --no-eff-email -d 158.255.1.190.sslip.io
+# echo "### Перезапуск nginx для применения сертификатов ###"
+# docker-compose -f docker-compose.prod.yml restart nginx
+# echo "### Готово ###"
+
+echo "### Certbot отключен ###"
+echo "### Для использования SSL необходимо настроить альтернативный способ получения сертификатов ###"
