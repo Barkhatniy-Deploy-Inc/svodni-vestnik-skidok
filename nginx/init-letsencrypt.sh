0cd3e50e3cadbccf96e862be634cafeb9a257db3
XYTRAX9 <yaroslav.dzhum@yandex.ru>
2026-02-17 11:25:54 +0500

    Правка в скрипте

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -2,10 +2,10 @@
 
 # Скрипт для инициализации Let's Encrypt сертификатов
 
-set -e
+# set -e
 
 echo "### Запуск certbot для получения сертификатов ###"
-docker-compose -f docker-compose.prod.ghrc.yml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email admin@89.232.177.172.sslip.io --agree-tos --no-eff-email -d 89.232.177.172.sslip.io
+docker compose -f ../docker-compose.prod.ghrc.yml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email admin@89.232.177.172.sslip.io --agree-tos --no-eff-email -d 89.232.177.172.sslip.io
 echo "### Перезапуск nginx для применения сертификатов ###"
-docker-compose -f docker-compose.prod.ghrc.yml restart nginx
+# docker compose -f ../docker-compose.prod.ghrc.yml restart nginx
 echo "### Готово ###"
