b96651f9aa526056e676eed6dd9edb8f51b407a9
XYTRAX9 <yaroslav.dzhum@yandex.ru>
2026-02-17 10:56:42 +0500

    Временно убран редирект на HTPPS

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -6,7 +6,7 @@
 # set -e
 
 # echo "### Запуск certbot для получения сертификатов ###"
-# docker-compose -f docker-compose.prod.yml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email admin@158.255.1.190.sslip.io --agree-tos --no-eff-email -d 158.255.1.190.sslip.io
+# docker-compose -f docker-compose.prod.yml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email admin@89.232.177.172.sslip.io --agree-tos --no-eff-email -d 89.232.177.172.sslip.io
 # echo "### Перезапуск nginx для применения сертификатов ###"
 # docker-compose -f docker-compose.prod.yml restart nginx
 # echo "### Готово ###"
